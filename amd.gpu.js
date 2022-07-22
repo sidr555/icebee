089c3bfc12b36049768e89d0ac7c2b63957c772a
Dmitry Sidorov <sidr@sidora.net>
2022-07-22 23:34:28 +0800

    overclock amd worker

--- amd.gpu.js
+++ amd.gpu.js
@@ -1,14 +1,19 @@
 const GPU = require("./gpu")
 
 class AmdGPU extends GPU {
+    temp = {
+        max: 57,
+        critical: 62
+    }
+    
+    oc_keys = ["core_clock", "mem_clock"]
 
-    maxTemp = 50;
     core_clock = {
-        min: 1200,
-        max: 2200,
+        min: 600,
+        max: 1200,
         step: {
             up: 50,
-            down: 200
+            down: 100
         }
     }
     mem_clock = {
@@ -19,6 +24,8 @@ class AmdGPU extends GPU {
             down: 200
         }
     }
+
+    oc_keys = ["core_clock", "mem_clock"];
     
     getCommonDataParams() {
         return {
@@ -34,24 +41,32 @@ class AmdGPU extends GPU {
         }
     }
     getOverclockParams() {
-        const oc = {
-            core_clock: this.oc.amd.core_clock[this.index],
-            mem_clock: this.oc.amd.mem_clock[this.index],
-            core_state: this.oc.amd.core_state[this.index],
-            fan_speed: this.oc.amd.fan_speed ? this.oc.amd.fan_speed[this.index] : null,
-            power_limit: this.oc.amd.power_limit ? this.oc.amd.power_limit[this.index] : null
-        }
+        // const oc = {
+        //     core_clock: this.oc.amd.core_clock[this.index],
+        //     mem_clock: this.oc.amd.mem_clock[this.index],
+        //     // core_state: this.oc.amd.core_state[this.index],
+        //     // fan_speed: this.oc.amd.fan_speed ? this.oc.amd.fan_speed[this.index] : null,
+        //     // power_limit: this.oc.amd.power_limit ? this.oc.amd.power_limit[this.index] : null
+        // }
+
+        const oc = {}
+        let core_clock = this.oc.amd.core_clock[this.index];
+        let mem_clock = this.oc.amd.mem_clock[this.index];
+
+        oc.mem_clock = mem_clock;
+
         if (this.isOverheated()) {
-            if (oc.core_clock > this.core_clock.min) {
-                const core_clock = oc.core_clock - this.core_clock.step.down;
-                oc.core_clock = core_clock >= this.core_clock.min ? core_clock : this.core_clock.min;  
+            console.log(`GPU (a) w:${this.worker} #${this.index} is overheated ${this.stat.temp} (core_clock: ${core_clock}, mem_clock: ${mem_clock}, power: ${this.stat.power})`);
+            if (core_clock > this.core_clock.min) {
+                core_clock -= this.core_clock.step.down;
+                oc.core_clock = core_clock >= this.core_clock.min ? core_clock : this.core_clock.min; 
             } else {
                 console.log("Cannot power down amd GPU", this.info);
                 return false;
             }
         } else {
-            if (oc.core_clock < this.core_clock.max) {
-                const core_clock = oc.core_clock + this.core_clock.step.up;
+            if (core_clock < this.core_clock.max) {
+                core_clock += this.core_clock.step.up;
                 oc.core_clock = core_clock <= this.core_clock.max ? core_clock : this.core_clock.max;  
             } else {
                 console.log("Cannot power up amd GPU", this.info);
